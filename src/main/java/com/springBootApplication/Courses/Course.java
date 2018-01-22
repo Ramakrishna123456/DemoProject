5442f2a5c5082fff966e5e7a99f61df1b4992070
Ramakrishna Valavala <Ramakrishna Valavala@BZHP4M2>
2018-01-22 15:03:31 -0700

    First Commit

---
+++ src/main/java/com/springBootApplication/Courses/Course.java
@@ -0,0 +1,59 @@
+package com.springBootApplication.Courses;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.springBootApplication.Topic.Topic;
+
+@Entity
+public class Course {
+	
+	@Id
+	private String Id;
+	private String name;
+	private String description;
+	
+	@ManyToOne
+	private Topic topic;
+	
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
+	public Course() {}
+	
+	public Course(String id, String name, String description, String topicId) {
+		super();
+		this.Id = id;
+		this.name = name;
+		this.description = description;
+		this.topic = new Topic(topicId, "","");
+		
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
